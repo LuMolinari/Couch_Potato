f9c89f4b3ba48d3efc3fc2d2f4a8bc5dda9b4bbf
Luca <lucamolinari1@gmail.com>
2020-10-17 21:28:50 -0700

    Linked Sign in button, as well as established fragment

--- app/src/main/java/com/example/couchpotato/MainActivity.java
+++ app/src/main/java/com/example/couchpotato/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.couchpotato;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signInButton = (Button) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), nav_bar_layout.class);
+                view.getContext().startActivity(intent);}
+        });
     }
 }
+
