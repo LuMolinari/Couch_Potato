e123cf02d713244ec92a81ca12ac51545408673e
Luca <lucamolinari1@gmail.com>
2020-10-29 19:03:29 -0700

    Added Trakt integration via API library as well as finished the settings activities

--- app/src/main/java/com/example/couchpotato/MainActivity.java
+++ app/src/main/java/com/example/couchpotato/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.couchpotato;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuthenticationManager firebaseAuthenticationManager;
@@ -10,6 +11,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseAuthenticationManager = new FirebaseAuthenticationManager();
+
+        //The example used to display api functionality
+
+//        TraktV2 trakt = new TraktV2("d07305deec2882a4e7b2bdafba909ed07cc94b00ffd82bf1919f67526dd550c5");
+//        Shows traktShows = trakt.shows();
+//        System.out.println("looking for Trakt");
+//        try {
+//            // Get trending shows
+//            Response<List<TrendingShow>> response = traktShows.trending(1, null, Extended.FULL).execute();
+//            if (response.isSuccessful()) {
+//                System.out.println("Successfully reached Trakt");
+//                List<TrendingShow> shows = response.body();
+//                for (TrendingShow trending : shows) {
+//                    System.out.println("Title: " + trending.show.title);
+//                }
+//            } else {
+//                if (response.code() == 401) {
+//                    // authorization required, supply a valid OAuth access token
+//                    System.out.println("invalid token for Trakt");
+//                } else {
+//                    // the request failed for some other reason
+//                    System.out.println("what happened Trakt");
+//                }
+//            }
+//        } catch (Exception e) {
+//            // see execute() javadoc
+//        }
 
 
     }
@@ -54,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                      let me know if you wan me to specify the reasons
                      Write below this comment whatever you want the app to do
                      when log in fails
+                     //TODO I think we could have a toast come up saying account not found, retry or signup
                      */
 
                 } else {
@@ -62,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
                     Write below this comment whatever you want the app to do
                      when log in is successful
+                     //todo from here we open up the main discover window filled with generally popular movies. i know trakt shows trending shows or movies
                      */
 
                 }
