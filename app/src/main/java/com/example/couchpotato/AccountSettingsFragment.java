ae5513f22d4a2c7a50c68b8b8459518c2b0d7fd0
Edgar Mancia <eddiemm@users.noreply.github.com>
2020-11-09 17:39:59 -0800

    Add back button functionality to setting page

--- app/src/main/java/com/example/couchpotato/AccountSettingsFragment.java
+++ app/src/main/java/com/example/couchpotato/AccountSettingsFragment.java
@@ -4,16 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AccountSettingsFragment extends Fragment {
+
+    ImageView btn_back;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.account_settings_fragment, container, false);
+        btn_back = v.findViewById(R.id.back_button);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = getActivity()
+                        .getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new MyAccountFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
 
         return v;
     }
