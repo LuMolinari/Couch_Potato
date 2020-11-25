e8c68d015a328c4ba870e8c5b34ded65aababa3e
Luca <lucamolinari1@gmail.com>
2020-11-24 19:02:44 -0800

    favorites page almost completed

--- app/src/main/java/com/example/couchpotato/FavoritesFragment.java
+++ app/src/main/java/com/example/couchpotato/FavoritesFragment.java
@@ -1,6 +1,9 @@
 package com.example.couchpotato;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +19,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class FavoritesFragment extends Fragment {
@@ -27,6 +34,11 @@ public class FavoritesFragment extends Fragment {
     RecyclerView recyclerView;
     private DatabaseManager databaseManager;
     private FirebaseAuth mAuth;
+    private final ArrayList<String> movieId = new ArrayList<>();
+    private final ArrayList<BookmarkItem> favoritesList = new ArrayList<>();
+    ProgressDialog pd;
+    private String JSON_URL;
+    private GetData getData;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
@@ -36,6 +48,7 @@ public class FavoritesFragment extends Fragment {
 
         databaseManager = new DatabaseManager();
         mAuth = FirebaseAuth.getInstance();
+        getData = new GetData();
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +60,6 @@ public class FavoritesFragment extends Fragment {
             }
         });
 
-        ArrayList<BookmarkItem> favoritesList = new ArrayList<>();
         //get list of movies in MovieFavorites Directory.
         String documentPath = "users/" + mAuth.getCurrentUser().getUid() + "/Movies/FavoriteMovies";
         databaseManager.getDocumentSnapshot(documentPath, new FirebaseCallback() {
@@ -55,57 +67,20 @@ public class FavoritesFragment extends Fragment {
             public void callBack(Object status) {
                 DocumentSnapshot snapshot = (DocumentSnapshot) status;
                 //loop through movie ids in document
-                ArrayList<BookmarkItem> favoritesList = new ArrayList<>();
                 for (Object ds : snapshot.getData().values()) {
-                    //call api based on movie id
 
-                    String JSON_URL = "https://api.themoviedb.org/3/movie/"+  ds.toString() +"?api_key=4517228c3cc695f9dfa1dcb4c4979152&language=en-US&";
-                    try {
-                        //todo fix the way json is accessed
-                        JSONObject jsonObject = new JSONObject(JSON_URL);
+                    //create query for api with id string.
+                     JSON_URL = "https://api.themoviedb.org/3/movie/"+  ds.toString() +"?api_key=4517228c3cc695f9dfa1dcb4c4979152&language=en-US&";
+                     //call asynchronous class that will fetch the data
+                     getData.execute(JSON_URL);
 
-                        favoritesList.add(new BookmarkItem(jsonObject.getInt("poster_path"), jsonObject.getString("title"),
-                                jsonObject.getString("release_date"), jsonObject.getString("vote_average")));
-
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
                 }
             }
         });
 
-
-        PutDataIntoRecyclerView(favoritesList);
-
-
-
-        //use id to get model by calling api
-
-        //fill relevant fields in model
-
-        //pass the model to create item bookmark
-
-        //load bookmark into recycler view
-
-
-
-
-
-
-
-
-
-
-
-
-
         return v;
 
     }
-
-
-
 
 
 
@@ -118,8 +93,71 @@ public class FavoritesFragment extends Fragment {
 
         recyclerView.setAdapter(bookmarkAdapter);
 
-
     }
 
+
+
+    //create private getdata class for private json calls
+    public class GetData extends AsyncTask<String, String, String> {
+
+
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            System.out.println(result);
+            //result is our actual json data.
+            // Now turn it into a json object and use the objects to fill the favorites page.
+
+        }
+    }
 
 }
